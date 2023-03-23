1bba63c333bd0077eb260c432738420e1b508309
rivainasution <rivai_nasution@student.ub.ac.id>
2023-03-23 08:43:14 +0700

    add gh-pages

--- src/pages/detail/components/Price/Price.js
+++ src/pages/detail/components/Price/Price.js
@@ -14,6 +14,7 @@ import {
 //TODO: Start Function
 export default function Price ({data, currencySymbol, rates, symbol}){
     const [add, setAdd] = useState(false);
+    
     const WatchlistHandler = () => {
         if (add === false){
             return setAdd(true);
